5cedc65008aafc5bf769fce675721f874207e9f8
rconjoe <joe@rconjoe.com>
2022-02-25 13:15:24 -0800

    student factory with faker data

--- database/factories/StudentFactory.php
+++ database/factories/StudentFactory.php
@@ -9,15 +9,19 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class StudentFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition()
-    {
-        return [
-            //
-        ];
-    }
+  /**
+   * Define the model's default state.
+   *
+   * @return array<string, mixed>
+   */
+  public function definition()
+  {
+    return [
+      'studentId' => $this->faker->numerify('#############'),
+      'firstName' => $this->faker->firstName(),
+      'lastName' => $this->faker->lastName(),
+      'email' => $this->faker->email(),
+      'present' => $this->faker->boolean(75)
+    ];
+  }
 }
