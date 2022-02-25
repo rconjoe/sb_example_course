8c5fe897fd5da8df13de6a3ce39b8887b1bc6938
rconjoe <joe@rconjoe.com>
2022-02-25 13:47:13 -0800

    init header

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="mt-8">
+  <div class="">
+      <nav class="p-6 bg-gray-200 flex justify-between mx-4 mb-6 mt-5 rounded lg">
+      <div class="">Attendance Record</div>
+      <a href="https://github.com/rconjoe/sb_example_course" class="text-gray-600 no-underline hover:underline">Source</a>
+    </nav>
   </div>
 </template>
 
