9e297b5fbac7c9f2c760b6deccdaca717466c0d0
rconjoe <joe@rconjoe.com>
2022-02-25 12:14:40 -0800

    init vue app and default component

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,15 @@
-require('./bootstrap');
+require('./bootstrap')
+
+// Import vue createApp method and top level component
+import { createApp } from 'vue'
+import App from './components/App.vue'
+
+// Create vue app
+const app = createApp({})
+
+// Register vue components
+app.component('App', App)
+
+// Mount vue app
+app.mount('#app')
+
---
+++ resources/js/components/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="mt-8">
+  </div>
+</template>
+
+  <script>
+  export default {
+    mounted() {
+      console.log('mounteddd')
+    }
+  }
+  </script>
