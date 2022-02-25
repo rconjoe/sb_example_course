1a7c04fd5da9f514aa47fbd148593c08ac576d3c
rconjoe <joe@rconjoe.com>
2022-02-25 13:48:11 -0800

    make margin uniform around header instead of mx-mt-mb

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="">
-      <nav class="p-6 bg-gray-200 flex justify-between mx-4 mb-6 mt-5 rounded lg">
+      <nav class="p-6 bg-gray-200 flex justify-between m-5 rounded lg">
       <div class="">Attendance Record</div>
       <a href="https://github.com/rconjoe/sb_example_course" class="text-gray-600 no-underline hover:underline">Source</a>
     </nav>
