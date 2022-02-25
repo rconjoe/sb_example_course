79800d5c193f104c6a36be67babbb16b58724b70
rconjoe <joe@rconjoe.com>
2022-02-25 15:00:20 -0800

    import and render StudentTable

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="">
-      <nav class="p-6 bg-gray-200 flex justify-between m-5 rounded lg">
+      <nav class="p-6 bg-gray-200 flex justify-between my-5 mx-16 rounded lg">
       <div class="">Attendance Record</div>
       <a href="https://github.com/rconjoe/sb_example_course" class="text-gray-600 no-underline hover:underline">Source</a>
     </nav>
   </div>
+      <StudentTable />
 </template>
 
-  <script>
-  export default {
-    mounted() {
-      console.log('mounteddd')
-    }
+<script>
+import StudentTable from './StudentTable.vue'
+
+export default {
+  components: {
+    StudentTable
   }
-  </script>
+}
+</script>
