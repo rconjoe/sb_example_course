2513cf0e86c22c77e265bd8f405d123105525a8d
rconjoe <joe@rconjoe.com>
2022-02-25 13:15:57 -0800

    init get, post, put routes for students

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Student;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,5 +16,27 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+  return $request->user();
+});
+
+Route::get('/students', function () {
+  return Student::all();
+});
+
+Route::post('/students', function () {
+  Student::create([
+    'studentId' => request('studentId'),
+    'firstName' => request('firstName'),
+    'lastName' => request('lastName'),
+    'email' => request('email'),
+    'present' => request('present'),
+  ]);
+});
+
+Route::put('/students/{studentId}', function ($id) {
+
+  $student = Student::find($id);
+  $student->update([
+    'present' => request('present')
+  ]);
 });
